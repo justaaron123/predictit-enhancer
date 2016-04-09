7d2a3f7326b15c3b96b04a961455b383cc58583c
curmil <curmil@github.com>
2016-04-09 13:35:09 +0800

    links in the my shares drop down now work. The scripts for the drop down are now appended in the page on load so they're not bound by some of the chrome restrictions for content scripts

---
+++ loadTopDown.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    addScript('MySharesOrdering.js');
+    addScript('TopDownMenu2.js');
+
+    //contract page does not have the myshares css so load it
+    addCss('https://www.predictit.org/Content/css/myShares.css');
+});
