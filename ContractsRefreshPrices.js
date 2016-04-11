daec0c81471b6b07e9aa7bbf03dbe3b73ff6cde7
curmil <curmil@github.com>
2016-04-12 05:07:33 +0800

    fixing conflicting refresh functionality between contract list and price list

--- ContractsRefreshPrices.js
+++ ContractsRefreshPrices.js
@@ -1,39 +1,100 @@
 $(document).ready(function(){
     var autoRefresh = false;
 
+    $('body').on('click', '#refreshPrices', refreshPrices);
+
     $('body').on('click', 'a.refresh', function() {
         setTimeout(function() {
             appendCheckbox();
-        }, 1200);
+        }, 1450);
     });
 
-    $('body').on('click', '#refreshPrices', refreshPrices);
-
     function appendCheckbox() {
+        if ($('#refreshPrices').length > 0) {
+            $('#refreshPrices').remove();
+        }
+
         $('<span> \
             <input id="refreshPrices" type="checkbox" \
             name="autorefresh" value="1" style="margin: 0 3px;" \
             ' + ((autoRefresh) ? 'checked="checked"' : '') + '> \
             <label style="margin: 0px;" for="refreshPrices">Auto Refresh</label> \
             </span>').appendTo('th.contract-title');
+
+    }
+
+
+    window.addEventListener("message", function(event) {
+      // We only accept messages from ourselves
+      if (event.source != window)
+        return;
+    
+      if (event.data == 'pauseRefresh') {
+            $('#refreshPrices').addClass('pause');
+      }
+
+      if (event.data == 'unpauseRefresh') {
+
+          setTimeout(function() {
+              if ($('#refreshPrices').hasClass('pause') && !$('.showPrice').length) {
+                  $('#refreshPrices').removeClass('pause');
+
+                  refreshPrices();
+              }
+          }, 1000);
+      }
+
+    }, false);
+
+    function preventRefresh($refresher) {
+        if (!$refresher.is(':checked')) { 
+            autoRefresh = false;
+            return true; 
+        }
+
+        if (!$refresher.is(':checked') || $refresher.hasClass('refreshing') || $refresher.hasClass('pause')) { 
+            return true;
+        }
+
+        return false;
     }
 
     function refreshPrices() {
         $refresher = $('#refreshPrices');
 
-        if (!$refresher.is(':checked')) { 
-            autoRefresh = false;
+        if (preventRefresh($refresher)) { 
             return; 
         }
 
-        $('span.sharesUp, span.sharesDown').trigger('mouseleave');
+        $('span.sharesUp, span.sharesDown').removeClass('showPrice');
 
         autoRefresh = true;
 
-        $('a.refresh').trigger('click');
+        $refresher.addClass('refreshing');
 
         setTimeout(function() {
-            refreshPrices();
+
+            $refresher.removeClass('refreshing');
+
+            if (preventRefresh($refresher)) { 
+                return;
+            }
+
+            $('a.refresh').first().trigger('click');
+
+        }, 300);
+
+
+        $refresher.addClass('refreshing');
+
+        setTimeout(function() {
+
+            $refresher.removeClass('refreshing');
+
+            if (!preventRefresh($refresher)) { 
+                refreshPrices();
+            }
+
         }, 6500);
 
     }
